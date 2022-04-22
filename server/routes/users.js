fa727e5c872a7ce53e92338c5a92f112cb86646c
maddclif <maddclif24@gmail.com>
2022-04-22 22:37:59 +0300

    Add ability user edit

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,20 +13,24 @@ export default (app) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
     })
-    .patch('/users/:id', async (req, reply) => {
-      try {
-        const validUser = await app.objection.models.user.fromJson(req.body.data);
-        await app.objection.models.user.query().update(validUser).where('id', req.params.id);
-        reply.redirect(app.reverse('/users'));
-      } catch (error) {
-        console.log(error);
-      }
-      return reply;
-    })
     .get('/users/:id/edit', async (req, reply) => {
       const user = await app.objection.models.user.query().findById(req.params.id);
-      console.log(user);
       reply.render('users/edit', { user });
+      return reply;
+    })
+    .patch('/users/:id', { name: 'userEdit' }, async (req, reply) => {
+      const users = await app.objection.models.user.query();
+
+      try {
+        const { params, body } = req;
+        const user = await app.objection.models.user.query().findById(params.id);
+        await user.$query().patch(body.data).findById(params.id);
+        req.flash('success', i18next.t('flash.users.edit.success'));
+        reply.redirect(app.reverse('users'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.users.edit.error'));
+        reply.render('users/index', { users });
+      }
       return reply;
     })
     .post('/users', async (req, reply) => {
@@ -35,7 +39,6 @@ export default (app) => {
 
       try {
         const validUser = await app.objection.models.user.fromJson(req.body.data);
-        // console.log(validUser);
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
