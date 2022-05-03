c3d9b534fc974e231fbc9ad254fb31589b370cad
maddclif <maddclif24@gmail.com>
2022-05-03 15:22:24 +0300

    Add ability edit and delete statuses

---
+++ server/models/Task.js
@@ -0,0 +1,23 @@
+// @ts-check
+const BaseModel = require('./BaseModel.cjs');
+
+module.exports = class Task extends BaseModel {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string', minLength: 1 },
+        statusId: { type: 'integer' },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+      },
+    };
+  }
+};
--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -11,10 +11,10 @@ export default (app) => {
     })
     .get('/statuses/new', { name: 'newStatus' }, async (req, reply) => {
       const status = await app.objection.models.status.query();
-      reply.render('statuses/edit', { status });
+      reply.render('statuses/newStatus', { status });
       return reply;
     })
-    .post('/statuses', { name: 'statusEdit' }, async (req, reply) => {
+    .post('/statuses', { name: 'createNewStatus' }, async (req, reply) => {
       const { body } = req;
       const status = new app.objection.models.status();
       status.$set(body.data);
@@ -25,7 +25,44 @@ export default (app) => {
         reply.redirect(app.reverse('statuses'));
       } catch ({ data }) {
         req.flash('error', i18next.t('flash.statuses.create.error'));
-        reply.render('statuses/edit', { status, errors: data });
+        reply.render('statuses/newStatus', { status, errors: data });
+      }
+      return reply;
+    })
+    .get('/statuses/:id/edit', async (req, reply) => {
+      const status = await app.objection.models.status.query().findById(req.params.id);
+      try {
+        reply.render('statuses/edit', { status });
+      } catch ({ data }) {
+        reply.redirect(app.reverse('root'));
+      }
+      return reply;
+    })
+    .patch('/statuses/:id', { name: 'editStatus' }, async (req, reply) => {
+      const statuses = await app.objection.models.status.query();
+      try {
+        const { params, body } = req;
+        const status = await app.objection.models.status.query().findById(params.id);
+        await status.$query().patch(body.data).findById(params.id);
+        req.flash('success', i18next.t('flash.statuses.edit.success'));
+        reply.redirect(app.reverse('statuses'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.statuses.edit.error'));
+        reply.render('statuses/index', { statuses });
+      }
+      return reply;
+    })
+    .delete('/statuses/:id', { name: 'deleteStatus' }, async (req, reply) => {
+      const statuses = await app.objection.models.status.query();
+      try {
+        const { params } = req;
+        const status = await app.objection.models.status.query().findById(params.id);
+        await status.$query().deleteById(params.id);
+        req.flash('success', i18next.t('flash.statuses.delete.success'));
+        reply.redirect(app.reverse('users'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.statuses.delete.error'));
+        reply.render('users/index', { statuses });
       }
       return reply;
     });
